e029a34531012ebc23f0e4d3ed01df613c8de123
瑀行 <dingguangren@126.com>
2022-02-25 15:55:22 +0800

    二分查找获取左边界

--- src/main/java/com/yuxing/algorithm/base/BinarySearch.java
+++ src/main/java/com/yuxing/algorithm/base/BinarySearch.java
@@ -12,6 +12,43 @@ public class BinarySearch {
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
         int i = binarySearch(nums, 2);
         System.err.println(i);
+
+        nums = new int[]{1, 2, 2, 2, 2, 3, 4, 5};
+        System.err.println(leftBoundaryByBinarySearch(nums, 2) == 1);
+
+        nums = new int[]{1, 2, 3, 4, 4, 4, 5, 6};
+        System.err.println(leftBoundaryByBinarySearch(nums, 4) == 3);
+    }
+
+
+
+    /**
+     * 二分查找获取左边界
+     */
+    public static int leftBoundaryByBinarySearch(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid - 1;
+            }
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+
+        if (left >= n || nums[left] != target) {
+            return -1;
+        }
+        return left;
     }
 
     public static int binarySearch(int[] nums, int target) {
